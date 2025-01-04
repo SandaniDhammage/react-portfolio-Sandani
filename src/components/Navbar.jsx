c9cbd9b777eb2c00202e4f917cdeade88983bb2a
SandaniDhammage <sandanid01@gmail.com>
2025-01-04 16:53:27 +0530

    Updated Contact and Navbar components for email link and navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,51 @@
 
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-import { FaInstagram} from "react-icons/fa";
-import { FiFigma } from 'react-icons/fi';
+import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
+import { FiFigma } from "react-icons/fi";
 
 const Navbar = () => {
-  return <nav className=" mb-20 flex items-center justify-between py-6 ">
-    <div className="flex flex-shrink-0 items-center">
-    <h1 className=" text-3xl font-normal tracking-wide mx-2 w-10">
-        <span className="text-4xl text-purple-200">S</span>ADEE
-    </h1>
-
-    </div>
-    <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-      <FaLinkedin/>
-      <FaGithub/>
-      <FaInstagram/>
-      <FiFigma/>
-    </div>
-  </nav>
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <h1 className="text-3xl font-normal tracking-wide mx-2 w-10">
+          <span className="text-4xl text-purple-200">S</span>ADEE
+        </h1>
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a 
+          href="https://www.linkedin.com/in/sandani-dhammage/"
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="hover:text-blue-500"
+        >
+          <FaLinkedin />
+        </a>
+        <a 
+          href="https://github.com/SandaniDhammage" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="hover:text-gray-700"
+        >
+          <FaGithub />
+        </a>
+        <a 
+          href="https://www.instagram.com/sadee01_/profilecard/?igsh=MXJoNG95dGNua3k2aQ==" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="hover:text-pink-500"
+        >
+          <FaInstagram />
+        </a>
+        <a 
+          href="https://www.figma.com/files/team/1320276785026890986/recents-and-sharing?fuid=1320276780474359856" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="hover:text-purple-500"
+        >
+          <FiFigma />
+        </a>
+      </div>
+    </nav>
+  );
 };
 
-export default Navbar
+export default Navbar;
